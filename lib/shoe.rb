c5cc38064dcdc5e9c80ab8329294fa85df011e1f
Marcus Lau <lau.marcus76@gmail.com>
2020-04-27 21:48:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -16,11 +16,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    if !BRANDS.include?(brand)
-      BRANDS << brand
-    end
-  end
-  
 end
